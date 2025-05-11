f02d6f4655ad0946099223258bd8044cc92798b4
Fedor Smirnov <fdor.smirnov.02@list.ru>
2025-05-11 20:12:35 +0300

    Подправил работу сеточного посиска коэффицентов PID

--- GA/GAGRIDPID.m
+++ GA/GAGRIDPID.m
@@ -1,31 +1,39 @@
 % Параметры системы
+% можно на Pythone, но нравится MATLAB
 g = 9.8;
 l = 0.151;
 m = 0.174;
-Km = 0.061; % крутизна двигателя
-R_ya = 5;   % сопротивление якоря
-L_ya = 670e-6; % индуктивность якоря
-b = 0.00064;   % коэффициент трения
+Km = 0.061;
+R_ya = 5;
+L_ya = 670e-6;
+b = 0.00064;
 
 J = m * l^2; % момент инерции
 a = m * g * l; % возвращающий момент
 
-% Передаточная функция объекта управления
+% Передаточная функция замкнутой системы
 num = [Km];
 den = [L_ya*J, L_ya*b + R_ya*J, L_ya*a + R_ya*b + Km^2, R_ya*a];
 
-sys = tf(num, den);
-lb = [0, 0, 0];
-ub = [1000, 1000, 1000];
-
-fitnessfcn = @(K) pidfitnessgrid(K, sys);
-
-% Пример одной настройки
+sys = tf(num, den);% Параметры генетического алгоритма
 options = optimoptions('ga', ...
-    'PopulationSize', 100, ...
+    'PopulationSize', 50, ...
     'MaxGenerations', 100, ...
-    'MutationFcn', {@mutationuniform, 0.1}, ...
-    'CrossoverFraction', 0.95, ...
-    'Display', 'iter');
+    'CrossoverFraction', 0.8, ...
+    'MutationFcn', {@mutationadaptfeasible, 0.1}, ...
+    'Display', 'iter', ...
+    'PlotFcn', {@gaplotbestf, @gaplotstopping});
 
-[Kopt, Jopt] = ga(fitnessfcn, 3, [], [], [], [], lb, ub, [], options);
+% Запуск генетического алгоритма
+[K_opt, fval] = ga(@(K) pidfitnessgrid(K, sys), 3, [], [], [], [], lb, ub, [], options);
+
+% Оптимальные коэффициенты
+Kp = K_opt(1);
+Ki = K_opt(2);
+Kd = K_opt(3);
+% Создаем оптимальный регулятор
+controller = pid(K_opt(1), K_opt(2), K_opt(3));
+closed_loop = feedback(controller * sys, 1);
+step(closed_loop);
+grid on;
+title('Переходная характеристика с оптимальным PID-регулятором');
--- GA/pidfitnessgrid.m
+++ GA/pidfitnessgrid.m
@@ -1,34 +1,37 @@
-function J = pidfitnessgrid(K, sys)
-    Kp = K(1); Ki = K(2); Kd = K(3);
-
+function fitness = pidfitnessgrid(K, sys)
+    % Извлекаем параметры PID
+    Kp = K(1);
+    Ki = K(2);
+    Kd = K(3);
+    
+    % Создаем PID-регулятор
     controller = pid(Kp, Ki, Kd);
+    
+    % Замкнутая система
     closed_loop = feedback(controller * sys, 1);
+    
+    % Симулируем переходную характеристику
+    [y, t] = step(closed_loop, 0:0.01:5);
+    
+    % Рассчитываем критерии качества
+    overshoot = max(y) - 1;  % Перерегулирование
+    settling_time = find_settling_time(t, y); % Время установления
+    
+    % Интегральный критерий ITAE
+    error = 1 - y;
+    itae = sum(t .* abs(error));
+    
+    % Фитнес-функция (чем меньше, тем лучше)
+    fitness = 0.4*itae + 0.3*overshoot + 0.3*settling_time;
+end
 
-    try
-        [y,t] = step(closed_loop, 20); % моделируем 20 секунд
-        e = 1 - y; % ошибка от заданного значения (step = 1)
-
-        IAE = trapz(t, abs(e)); % Интегральная ошибка
-        Ts_idx = find(y >= 0.9 & y <= 1.1, 1, 'last');
-        if isempty(Ts_idx)
-            Ts = Inf;
-        else
-            Ts = t(Ts_idx);
-        end
-
-        Mp = max(y) - 1;
-        if Mp < 0
-            Mp = 0;
-        end
-
-        % Целевая функция
-        w1 = 1;   % штраф за ошибку
-        w2 = 10;  % штраф за время реакции
-        w3 = 50;  % штраф за перерегулирование
-
-        J = w1*IAE + w2*Ts + w3*Mp;
-
-    catch
-        J = Inf; % если система не устойчива
+function st = find_settling_time(t, y)
+    % Находим время установления (в пределах 2% от установившегося значения)
+    final_value = y(end);
+    idx = find(abs(y - final_value) > 0.02*final_value, 1, 'last');
+    if isempty(idx)
+        st = 0;
+    else
+        st = t(idx);
     end
 end
